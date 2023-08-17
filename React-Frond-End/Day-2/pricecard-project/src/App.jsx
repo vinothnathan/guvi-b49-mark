a0cbb5b70973fad47ea8fa245571e854ac6c79d0
vino vrt <vinothnathan24@gmail.com>
2023-08-18 04:25:45 +0530

    React day-2 task

---
+++ React-Frond-End/Day-2/pricecard-project/src/App.jsx
@@ -0,0 +1,37 @@
+// App.js
+import React from 'react';
+import PriceCard from './PriceCard';
+
+const App = () => {
+  const plans = [
+    {
+      planName: 'Basic Plan',
+      dataLimit: 'Unlimited Data',
+      speed: '40 Mbps speed',
+      price: '499',
+      buttonText: 'Select Plan',
+    },
+    {
+      planName: 'Standard Plan',
+      dataLimit: 'Unlimited Data',
+      speed: '100 Mbps speed',
+      price: '799',
+      buttonText: 'Select Plan',
+    },
+    {
+      planName: 'Premium Plan',
+      dataLimit: 'Unlimited Data',
+      speed: '200 Mbps speed',
+      price: '999',
+      buttonText: 'Select Plan',
+    },
+  ];
+
+  const priceCards = plans.map((plan, index) =>
+    React.createElement(PriceCard, { key: index, ...plan })
+  );
+
+  return React.createElement('div', { className: 'app' }, priceCards);
+};
+
+export default App;
