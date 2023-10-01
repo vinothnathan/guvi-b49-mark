25bd6bc3fdec6c3bba8b62f24638c7d7831990d0
vino vrt <vinothnathan24@gmail.com>
2023-10-02 02:39:59 +0530

    Day-6 Task

---
+++ React Frond End/Day-6/my-app/src/FullStackDevelopment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FullStackDevelopment() {
+  return (
+    <div>
+      <h1>FullStackDevelopment</h1>
+    </div>
+  );
+}
+
+export default FullStackDevelopment;
